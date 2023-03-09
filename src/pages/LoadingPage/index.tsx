d82450f9b0ebd8f567d2c03a656ef9c875fb14dd
ptrv-dev <petrov99119@gmail.com>
2023-03-10 00:09:41 +0200

    Loading Page

---
+++ src/pages/LoadingPage/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import style from './LoadingPage.module.scss';
+
+const LoadingPage: React.FC = () => {
+  return (
+    <div className={style.loading}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="4"
+          r="35"
+          strokeDasharray="164.93361431346415 56.97787143782138"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+          ></animateTransform>
+        </circle>
+      </svg>
+    </div>
+  );
+};
+
+export default LoadingPage;
