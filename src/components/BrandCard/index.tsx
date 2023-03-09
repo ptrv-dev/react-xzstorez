070710fa641185b5d102d487c71958482625a8e4
ptrv-dev <petrov99119@gmail.com>
2023-03-09 22:15:12 +0200

    Brand Card

---
+++ src/components/BrandCard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { BrandItem } from '../../@types/serverResponse';
+
+import style from './BrandCard.module.scss';
+
+const BrandCard: React.FC<BrandItem> = ({ _id, title, image }) => {
+  return (
+    <Link to={`/all-items?category=${_id}`} className={style['card']}>
+      <img className={style['card__image']} src={`\\${image}`} alt={title} />
+      <h4 className={style['card__title']}>{title}</h4>
+    </Link>
+  );
+};
+
+export default BrandCard;
