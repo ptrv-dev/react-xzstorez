42d8ba5cd549969bf72c81e566300bd0339e4f6d
ptrv-dev <petrov99119@gmail.com>
2023-03-09 18:20:29 +0200

    custom hook useWindowSize added

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,22 @@
+import React, { useLayoutEffect, useState } from 'react';
+import { debounce } from '../utils/debounce';
+
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener(
+      'resize',
+      debounce(() => {
+        updateSize();
+      }, 500)
+    );
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
+
+export default useWindowSize;
