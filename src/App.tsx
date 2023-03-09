ff93c20725dd4e4eeb171e8fde1980054091f456
ptrv-dev <petrov99119@gmail.com>
2023-03-09 16:14:56 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+    </Routes>
+  );
+};
+
+export default App;
