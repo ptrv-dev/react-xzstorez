f9a94f9f11228f50f3750ec22532fcaa7bc698c8
ptrv-dev <petrov99119@gmail.com>
2023-03-09 22:03:52 +0200

    Types update

---
+++ src/@types/serverResponse.d.ts
@@ -0,0 +1,47 @@
+export interface BrandItem {
+  _id: string;
+  image: string;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+export interface BrandResponse {
+  data: BrandItem[];
+}
+
+export interface CategoryItem {
+  _id: string;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+
+export interface CategoryResponse {
+  data: CategoryItem[];
+}
+
+interface Price {
+  $numberDecimal: string;
+}
+
+export interface ProductItem {
+  _id: string;
+  images: string[];
+  title: string;
+  description?: string;
+  category?: CategoryItem;
+  brand?: BrandItem;
+  sizes?: string[];
+  price: Price;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+
+export interface ProductResponse {
+  data: ProductItem[];
+  page: number;
+  pagesCount: number;
+}
