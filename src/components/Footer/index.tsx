2aa9d3b322d009fcd5571b68c8cf0615fb851b50
ptrv-dev <petrov99119@gmail.com>
2023-03-11 12:20:28 +0200

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Logo from '../Logo';
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <Logo />
+        <p className="footer__copyright">2023 © XZstore</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
